e26403d6aea46951ae65fb439f0854167f2ddd83
Raruto <Raruto@users.noreply.github.com>
2023-12-11 11:58:43 +0100

    slim down

--- components/Chart.vue
+++ components/Chart.vue
@@ -8,88 +8,92 @@
 </template>
 
 <script>
-const { resizeMixin } = g3wsdk.gui.vue.Mixins;
+// const { XHR }      = g3wsdk.core.utils;
+
+// Fake Data
+const XHR = {
+  get: async function(url) {
+    console.log(url);
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+          resolve({
+            data:  [
+              {
+                x: [
+                  '2013-08-04 22:23:00',
+                  '2013-09-04 22:23:00',
+                  '2013-10-04 22:23:00',
+                  '2013-11-04 22:23:00',
+                  '2013-12-04 22:23:00',
+                ],
+                y: [4, 1, 7, 1, 4],
+                mode: 'markers',
+                type: 'scatter',
+                name: 'Scatter',
+                marker: { size: 5, color: 'black' }
+              },
+              {
+                x: [
+                  '2013-08-04 22:23:00',
+                  '2013-09-04 22:23:00',
+                  '2013-10-04 22:23:00',
+                  '2013-11-04 22:23:00',
+                  '2013-12-04 22:23:00',
+                ],
+                y: [3, 2, 1.5, 2, 4],
+                mode: 'lines',
+                name: 'Lin Trend',
+                marker: {
+                  color: 'red'
+                }
+              }
+            ],
+            layout: {
+              xaxis: {
+                title: '[Date]',
+                range: [ '2013-07-04 22:23:00', '2014-01-04 22:23:00' ]
+              },
+              yaxis: {
+                title: '[mm]',
+                range: [0, 8]
+              },
+              title:'Data Labels Hover'
+            },
+            config: {
+              displayModeBar: true,
+              editable: true,
+              responsive: true,
+              scrollZoom: true
+            }
+          })
+        }, 1000)
+      });
+  },
+};
 
 export default {
+
   name: "Chart",
+
   props: {
     fid: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
+
   data() {
     return {
-      loading: true, //need to show loading bar during get data from server
+      loading: true, // show loading bar while getting data from server
     }
   },
-  mixins: [resizeMixin],
-  methods: {
-    /**
-     * Resize method call when window is resized
-     */
-    resize() {
-      Plotly.Plots.resize(this.$refs.chart)
-    }
-  },
-  /*
-  * Need to configure for resizeMixin
-   */
-  beforeCreate() {
-    this.delayType = 'debounce';
-  },
+
   async created() {
-    //Fake Data
-    this.chart = await new Promise((resolve, reject) => {
-      setTimeout(() => {
-        resolve({
-          data:  [
-            {
-              x: [
-                '2013-08-04 22:23:00',
-                '2013-09-04 22:23:00',
-                '2013-10-04 22:23:00',
-                '2013-11-04 22:23:00',
-                '2013-12-04 22:23:00'],
-              y: [4, 1, 7, 1, 4],
-              mode: 'markers',
-              type: 'scatter',
-              name: 'Scatter',
-              marker: { size: 5, color: 'black' }
-            },
-            {
-              x: [
-                '2013-08-04 22:23:00',
-                '2013-09-04 22:23:00',
-                '2013-10-04 22:23:00',
-                '2013-11-04 22:23:00',
-                '2013-12-04 22:23:00'],
-              y: [3, 2, 1.5, 2, 4],
-              mode: 'lines',
-              name: 'Lin Trend',
-              marker: {
-                color: 'red'
-              }
-            }
-          ],
-          layout: {
-            xaxis: {
-              title: '[Date]',
-              range: [ '2013-07-04 22:23:00', '2014-01-04 22:23:00' ]
-            },
-            yaxis: {
-              title: '[mm]',
-              range: [0, 8]
-            },
-            title:'Data Labels Hover'
-          }
-        })
-      }, 1000)
-    })
-    Plotly.newPlot(this.$refs.chart, this.chart.data, this.chart.layout);
-    //set loading to false
+    const chart = await XHR.get('path/to/' + this.fid);
+    Plotly.newPlot(this.$refs.chart, chart.data, chart.layout, chart.config);
     this.loading = false;
   },
+
 }
 </script>
 
--- config/index.js
+++
@@ -1,6 +0,0 @@
-import {name} from '../package.json';
-import i18n from './i18n'
-export default {
-  i18n,
-  name
-}
